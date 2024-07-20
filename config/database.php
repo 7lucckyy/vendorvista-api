02395e9377386f53d5452a5aa8d33bf30225aad6
7lucckyy <7lucckyy6611@gmail.com>
2024-07-20 09:31:37 +0100

    switch to pgsql

--- config/database.php
+++ config/database.php
@@ -59,8 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
           'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET   SESSION sql_require_primary_key=0',
+                //PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                //PDO::MYSQL_ATTR_INIT_COMMAND => 'SET   SESSION sql_require_primary_key=0',
             ]) : [],
         ],
 
@@ -77,6 +77,10 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
+            'options' => extension_loaded('pdo_pgsql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET   SESSION sql_require_primary_key=0',
+            ]) : [],
         ],
 
         'sqlsrv' => [
