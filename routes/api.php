fb76ae60671a228a1fc3199fd4731d7935f41aac
7lucckyy <7lucckyy6611@gmail.com>
2024-08-04 17:05:48 +0100

    callback

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,6 @@ Route::group(['prefix' => '/v1'], function () {
 
 });
 Route::post('/payment/callback', [UpdateOrderPaymentStatusController::class, 'handle']);
-Route::post('/payment/webhook', [UpdateOrderPaymentStatusController::class, 'handle']);
 
 
 
