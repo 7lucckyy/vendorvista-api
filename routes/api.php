592584ecda5def475cbc01570c1c82b7b3c62048
7lucckyy <7lucckyy6611@gmail.com>
2024-08-04 16:58:30 +0100

    both catch

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::group(['prefix' => '/v1'], function () {
 
 });
 Route::post('/payment/callback', [UpdateOrderPaymentStatusController::class, 'handle']);
+Route::post('/payment/webhook', [UpdateOrderPaymentStatusController::class, 'handle']);
 
 
 
