e19eebb237640ff025672ad12846d1d4f3eb6e9c
7lucckyy <7lucckyy6611@gmail.com>
2024-08-04 16:04:57 +0100

    push debug webhook

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::group(['prefix' => '/v1'], function () {
    
 
 });
-Route::post('/payment/webhook', [UpdateOrderPaymentStatusController::class, 'handle']);
+Route::post('/payment/webhook',dd('Hello'));
 
 
 
