257f5cc94bb6b59686e03a237985e1ddcda0647c
7lucckyy <7lucckyy6611@gmail.com>
2024-08-12 02:43:49 +0100

    changed to post request

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::group(['prefix' => '/v1'], function () {
 
 });
 
-Route::get('/payment/webhook', [UpdateOrderPaymentStatusController::class, 'handle']);
+Route::post('/payment/webhook', [UpdateOrderPaymentStatusController::class, 'handle']);
 
 
 
