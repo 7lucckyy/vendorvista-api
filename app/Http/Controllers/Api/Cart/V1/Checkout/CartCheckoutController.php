113e5e0242bd7cd4b114c4360c31f8dab67c94a0
7lucckyy <7lucckyy6611@gmail.com>
2024-09-09 07:05:57 +0100

    added is empty method to cartItem check cartcheckout

--- app/Http/Controllers/Api/Cart/V1/Checkout/CartCheckoutController.php
+++ app/Http/Controllers/Api/Cart/V1/Checkout/CartCheckoutController.php
@@ -26,7 +26,7 @@ class CartCheckoutController extends Controller
 
         $cartItems = $this->cartActions->getCartItemsRecord($customerId);
 
-        if (empty($cartItems)) {
+        if ($cartItems === []) {
             return errorResponse('Cart is empty');
         }
 
