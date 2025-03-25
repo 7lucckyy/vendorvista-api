ee1e0a01a2b104ec758f0b690eb781e27447e626
7lucckyy <7lucckyy6611@gmail.com>
2025-03-25 12:01:16 +0100

    removed the relationship foreign to artisan customer

--- database/migrations/2024_04_28_142023_create_artisans_table.php
+++ database/migrations/2024_04_28_142023_create_artisans_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('artisans', function (Blueprint $table) {
             $table->uuid('id')->primary()->index();
             $table->string('customer_id');
-            $table->foreign('customer_id')->references('id')->on('customers');
             $table->longText('about');
             $table->string('service');
             $table->string('whatsapp_number');
