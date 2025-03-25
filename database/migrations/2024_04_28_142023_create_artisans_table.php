f2421e02fc065c873fea423df92fbdbdb96727ec
7lucckyy <7lucckyy6611@gmail.com>
2025-03-25 12:06:09 +0100

    change to uuid

--- database/migrations/2024_04_28_142023_create_artisans_table.php
+++ database/migrations/2024_04_28_142023_create_artisans_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('artisans', function (Blueprint $table) {
             $table->uuid('id')->primary()->index();
+            $table->uuid('customer_id');
             $table->foreign('customer_id')->references('id')->on('customers');
             $table->longText('about');
             $table->string('service');
